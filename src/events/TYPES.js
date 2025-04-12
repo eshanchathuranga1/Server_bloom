165f7a7ce5c3e7b996adb5402195399293f1743f
eshanchathuranga1 <eshanchathurangafernandoo@gmail.com>
2025-04-12 16:24:00 +0530

    add: implement event handling for socket connections and define event types

---
+++ src/events/TYPES.js
@@ -0,0 +1,9 @@
+module.exports = {
+    WATSAPP_CONNECTION: 'whatsapp.connection',
+    WHATSAPP_MESSAGES: 'whatsapp.messages',
+    SYSTEM_LOG: 'system.log',
+    SYSTEM_ERROR: 'system.error',
+    SYSTEM_INFO: 'system.info',
+    SYSTEM_WARNING: 'system.warning',
+    SYSTEM_DEBUG: 'system.debug',
+}
--- src/events/event_handler.js
+++ src/events/event_handler.js
@@ -1,8 +1,4 @@
-const EventEmitter = require('events');
-
-const ev = new EventEmitter(); // Create a global event emitter instance
-ev.setMaxListeners(0); // Set the maximum number of listeners to unlimited
-
+const TYPE = require('./TYPES');
 // Event handler for socket connection
 ev.on('connection', (socket) => {
     
